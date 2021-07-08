f4c4c2eae21f7095fff534783d51330f49dea4c7
= <westerovs@mail.ru>
2021-07-08 20:19:40 +0300

    complete observer

---
+++ 1_observer/src/components/controller.js
@@ -0,0 +1,35 @@
+import { observer } from './utils/observer.js'
+import { tasksData } from './modal.js'
+import { CreateCardTemplate } from './view.js'
+import { render } from './utils/render.js'
+
+const wrapper = document.querySelector('.wrapper')
+
+// -------------- render
+tasksData.forEach((item, index) => {
+    render(wrapper, CreateCardTemplate(item, index + 1))
+})
+
+const tasks = document.querySelectorAll('.task');
+
+const openTask = (e) => {
+    const target = e.target
+
+    if (!target.closest('.task')) return
+    if (target.classList.contains('task-edit')) return
+    observer.notify(closeCard)
+    
+    target.classList.add('task-edit')
+    console.log('open task')
+}
+
+const closeCard = () => {
+    console.log('close card')
+    
+    tasks.forEach(task => {
+        if (task.classList.contains('task-edit')) task.classList.remove('task-edit')
+    })
+}
+
+wrapper.addEventListener('click', openTask)
+observer.addObserver(closeCard)
