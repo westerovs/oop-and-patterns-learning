f14fa5f019a6640154abc0ade30ca580ddc3ac07
Александр <westerovs@mail.ru>
2021-09-01 17:32:57 +0300

    added immutable

--- 1_observer/1_pop-up-observer/src/components/utils/observer.js
+++ 1_observer/1_pop-up-observer/src/components/utils/observer.js
@@ -1,7 +1,7 @@
 export const observer = {
     // ключ в котором хранятся observer`Ы (т.е слушатели)
     _observers: [],
-    
+
     // метод добавляющий слушателей
     addObserver(cb) {
         this._observers.push(cb)
@@ -11,7 +11,7 @@ export const observer = {
     removeObserver(cb) {
         this._observers = this._observers.filter(item => item !== cb)
     },
-    
+
     // метод уведомляющий всех
     notify() {
         this._observers.forEach(item => item())
