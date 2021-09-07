9784fbbd29deb79f63479d8c03c0f1e020826084
Александр <westerovs@mail.ru>
2021-09-07 15:41:45 +0300

    complete class observer

---
+++ 1_observer/3_messages-class-observer/src/js/index.js
@@ -0,0 +1,17 @@
+import messageView from './message.js'
+import abstractObservable  from './observable.js'
+
+const observable = new abstractObservable()
+
+const push = (text) => () => new messageView().create(text)
+
+const messageNew = push('New message !')
+const messageChat = push('Новое сообщение в чате !')
+
+observable.add(messageNew)
+observable.add(messageChat)
+observable.remove(messageNew)
+
+const button = document.querySelector('.button')
+button.addEventListener('click', () => observable.notify())
+
